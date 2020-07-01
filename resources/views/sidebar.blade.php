86bf4a555b807345609519226d75eb4a75adc662
timotheus ingga pranata <trancexe@gmail.com>
2020-07-01 17:31:07 +0700

    Initial commit

---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,15 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+      <!-- Add icons to the links using the .nav-icon class
+           with font-awesome or any other icon font library -->
+      <li class="nav-item has-treeview">
+      <a href="{{route('datatables')}}" class="nav-link">
+          <i class="nav-icon fas fa-tachometer-alt"></i>
+          <p>
+            Data Tables
+            {{-- <i class="right fas fa-angle-left"></i> --}}
+          </p>
+        </a>
+      </li>
+    </ul>
+  </nav>
